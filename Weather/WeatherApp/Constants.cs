c74c3ed5074132dd8a85ea1f8658e95e80693f1a
opilane <opilane@HP-CZC6297B9X>
2022-05-31 11:43:59 +0300

    Add project files.

---
+++ Weather/WeatherApp/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherApp
+{
+    public static class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapAPIKey = "fd3ea34bb5eb7ae4faae0c8095446b88";
+    }
+}
